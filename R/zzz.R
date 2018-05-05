805a381faf87c88758ea2e0902be5e8430a8de65
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-05-05 16:28:06 +0200

    rm rmd

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-#thanks to rstudio/r2d3 for idea/code
-
-.onLoad <- function(...) {
-  if (requireNamespace("knitr", quietly = TRUE)) {
-    knit_engines <- get("knit_engines", envir = asNamespace("knitr"))
-    knit_engines$set(vl = knit_vl)
-  }
-}
