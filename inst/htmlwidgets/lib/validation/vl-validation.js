5bc395dc5812de9ce0eb25f25293010e1ba564db
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-05-28 22:35:05 +0200

    add compile

--- inst/htmlwidgets/lib/validation/vl-validation.js
+++ inst/htmlwidgets/lib/validation/vl-validation.js
@@ -1,7 +1,11 @@
 function validateVl(spec){
   var ajv = new Ajv({schemaId: "auto", jsonPointers: true});
-  var validate = ajv.addMetaSchema(jsonSchemaDraft06)
-  .compile(schema);
-  var v = validate(spec)
-  return v ? v : validate.errors
+  var validate = ajv.addMetaSchema(jsonSchemaDraft06).compile(schema);
+  var v = validate(spec);
+  return v ? v : validate.errors;
 }  
+
+function compileSpec(spec){
+  var vgSpec = vl.compile(spec).spec;
+  return vgSpec;
+}
